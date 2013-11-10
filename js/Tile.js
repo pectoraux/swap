a4b822b88fb75f27a234924c81758629d671bed7
Michael Yang <themichaelyang@gmail.com>
2013-11-09 21:37:30 -0500

    removed entities.js, moved classes into seperate files

---
+++ js/Tile.js
@@ -0,0 +1,79 @@
+var switchedTiles = [];
+switchedTiles.length = 10;
+
+var Tile = {
+	update: function() {},
+	onCollide: function(ai) {},
+	blocksMovement: false,
+	color: "white",
+};
+
+var WhiteTile = function(){};
+WhiteTile.prototype = Object.create(Tile);
+
+var RedTile = function(){};
+RedTile.prototype = Object.create(Tile);
+RedTile.prototype.onCollide = function(ai) {
+	world.death();
+}
+RedTile.prototype.color = "red";
+
+var GreenTile = function(){};
+GreenTile.prototype = Object.create(Tile);
+GreenTile.prototype.onCollide = function(ai) {
+	world.victory();
+}
+GreenTile.prototype.color = "green";
+
+var GrayTile = function(){};
+GrayTile.prototype = Object.create(Tile);
+GrayTile.prototype.blocksMovement = true;
+GrayTile.prototype.color = "grey";
+
+var SwitchedTile = function(){};
+SwitchedTile.prototype = Object.create(Tile);
+SwitchedTile.prototype.blocksMovement = true;
+SwitchedTile.prototype.color = "orange";
+
+var SwitchTile = function(id){
+	this.switchingId = id;
+}
+SwitchTile.prototype = Object.create(Tile);
+SwitchTile.prototype.color = "yellow";
+SwitchTile.prototype.onCollide = function(ai) {
+	this.down = true;
+	switchedTiles[this.switchingId].blocksMovement = false;
+	switchedTiles[this.switchingId].color = "white";
+}
+SwitchTile.prototype.update = function() {
+	if(!this.down) {
+		switchedTiles[this.switchingId].blocksMovement = true;
+		switchedTiles[this.switchingId].color = "orange";
+	}
+	this.down = false;
+}
+
+var getTile = function(id) {
+	if(19<id && id<30) {
+		t = new SwitchedTile()
+		switchedTiles[id-20] = t;
+		return t;
+	}
+	else if(9<id && id<20) {
+		return new SwitchTile(id-10);
+	}
+	else {
+		switch(id) {
+			case 1:
+				return new GrayTile();
+			case 2:
+				return new GreenTile();
+			case 3:
+				return new RedTile();
+			default:
+				return new WhiteTile();
+		}
+	}
+}
+
+
