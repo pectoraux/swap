1b0f743b8d3d928d6796df3db10ce6438ae51021
Michael Yang <themichaelyang@gmail.com>
2013-11-24 00:38:25 -0500

    Cleaned up ai code, added LeftTurnRightAI

--- js/ai.js
+++ js/ai.js
@@ -9,13 +9,17 @@ var AI = {
 		this.y = y;
 	},
 	update: function(gridSize) {
-		if(this.vx!=0)
-			this.x += gridSize/this.vx;
-		if(this.vy!=0)
-			this.y += gridSize/this.vy; 
+		if(!this.hitWall) {
+			if(this.vx!=0)
+				this.x += gridSize / this.vx;
+			if(this.vy!=0)
+				this.y += gridSize / this.vy;
+		}
+		this.hitWall = false;
 	}, 
 	onCollide: function(tile) {
-
+		if(tile.blocksMovement)
+			this.hitWall = true;
 	},
 };
 
@@ -26,36 +30,24 @@ StationaryAI.prototype = Object.create(AI);
 
 var RightAI = function(x, y) {
 	this.init(x, y);
-}
-RightAI.hitWall = false;
-RightAI.prototype = Object.create(AI);
-RightAI.prototype.vx = 7;
-RightAI.prototype.update = function(gridSize) {
-	if(!this.hitWall) {
-		if(this.vx!=0)
-			this.x += gridSize/this.vx;
-		if(this.vy!=0)
-			this.y += gridSize/this.vy; 
-	}
+	this.vx = 7;
 	this.hitWall = false;
 }
-RightAI.prototype.onCollide = function(tile){
-	if(tile.blocksMovement)
-		this.hitWall = true;
-}
+RightAI.prototype = Object.create(AI);
 
 var RightBounceAI = function(x, y) {
 	this.init(x, y);
+	this.vx = 7;
+	this.hitWall = false;
 }
 RightBounceAI.prototype = Object.create(AI);
-RightBounceAI.prototype.vx = 7;
-RightBounceAI.hitWall = false;
 RightBounceAI.prototype.onCollide = function(tile) {
 	if(tile.blocksMovement && !this.hitWall) {
 		this.vx *= -1;
 		this.hitWall = true;
 	}
 }
+
 RightBounceAI.prototype.update = function(gridSize) {
 	this.hitWall = false;
 
@@ -74,12 +66,38 @@ FollowAI.prototype.update = function(gridSize) {
 	this.y += player.getVelocity().y;
 }
 FollowAI.prototype.onCollide = function(tile) {
-	if(tile.blocksMovement) 
-		world.death();
+	// fix this!
+	this.vx *= -0.3;
+	this.vy *= -0.3;
 }
 
-var LeftTurnAI = function(x, y) {
+var LeftTurnRightAI = function(x, y) {
 	this.init(x, y);
+	this.vx = 7;
+}
+LeftTurnRightAI.prototype = Object.create(AI);
+LeftTurnRightAI.prototype.update = function(gridSize) {
+	if(this.vx!=0)
+		this.x += gridSize/this.vx;
+	if(this.vy!=0)
+		this.y += gridSize/this.vy; 
+	this.hitWall = false;
+}
+LeftTurnRightAI.prototype.onCollide = function(tile) {
+	// fix this! collision is quite funky
+	// goes into wall before changing dir, so onCollide still runs
+	// making it go in a U turn
+	if(tile.blocksMovement && !this.hitWall) {
+		this.vxo = this.vx;
+		this.vyo = this.vy;
+		this.vx = 0;
+		this.vy = 0;
+		if (this.vxo > 0) this.vy = -7;
+		else if (this.vxo < 0) this.vy = 7; 
+		else if (this.vyo > 0) this.vx = 7;
+		else if (this.vyo < 0) this.vx = -7;
+	}
+	this.hitWall = true;
 }
 
 var getAI = function(x, y, id) {
@@ -92,5 +110,7 @@ var getAI = function(x, y, id) {
 			return new RightBounceAI(x, y);
 		case -4: 
 			return new FollowAI(x, y);
+		case -5:
+			return new LeftTurnRightAI(x, y);
 	}
 }
