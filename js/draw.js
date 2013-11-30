a2097f89c77bb8a6876e164e4f7b85a61da27603
Noah Moroze <noahm@moroze.com>
2013-11-30 10:17:44 -0500

    Did bad stuff to actually fix centering

--- js/draw.js
+++ js/draw.js
@@ -26,7 +26,9 @@ var renderer = function() {
 	}
 
 	var renderText = function(deaths, level, tip) {
-		hud.innerHTML = deaths + " Deaths " + " | Level " + (level+1) + " / " + levels.length + " <sup><a style='font-size: 12px; text-decoration: none;' href='#"+(level+1)+"'>(link)</a></sup>";
+						//it pains me to do this, but it's necessary
+		hud.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
+		+deaths + " Deaths " + " | Level " + (level+1) + " / " + levels.length + " <sup><a style='font-size: 12px; text-decoration: none;' href='#"+(level+1)+"'>(link)</a></sup>";
 		tipDisplay.innerHTML =  "<em>" + tip + "</em>";
 	}
 
